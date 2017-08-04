fb90cd60d8e9f8813a4fabbfffdbb7e3124fcf9d
Antti Myyrä <antti.myyra@codento.com>
2017-08-04 19:29:29 +0300

    Tag search works \o/

--- api/api.js
+++ api/api.js
@@ -47,7 +47,58 @@ export function startServer() {
 
   })
 
+  server.get('/api/events/:party/tags/:tags', (req, res, next) => {
+    if (req.params.party.length < 3 || req.params.tags.length < 2) {
+      log.error(new Date() + ' Party ' + req.params.party + ' or tags + ' + req.params.tags  + ' does not match requirements and cannot exist.');
+      res.send(404, 'Defined party or tags do not meet requirements: ' + req.params.party);
+    }
+
+    var tags = [];
+
+    if (req.params.tags.includes('+')) {
+      req.params.tags.split('+').forEach((tag) => {
+        tags.push(tag);
+      });
+    }
+    else {
+      tags.push(req.params.tags);
+    }
+    
+    models.event.findAll({
+      where: { 
+        party: req.params.party,
+        tags: { $like: '%' + tags[0] + '%' }
+      }
+    }).then((events) => {
+      // If we have more tags, we'll filter out the ones without all of them.
+      if (tags.length > 1) {
+        var tempevents = [];
+        events.forEach((event) => {
+          var included = true;
+          tags.forEach((tag) => {
+            if (!event.tags.includes(tag)) included = false;
+          })
+          if (included) {
+            tempevents.push(event);
+          }
+        })
+        events = tempevents;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(200, events);
+    }).catch((err) => {
+      log.error(new Date() + ' Error when fetching events for party ' + req.params.party + ' with tags ' + req.params.tags + ': ' + err);
+      res.send(500, 'Error when fetching events for party ' + req.params.party);
+    })
+  })
+
   server.get('/api/events/:party', (req, res, next) => {
+    if (req.params.party.length < 3) {
+      log.error(new Date() + ' Party ' + req.params.party + ' does not match requirements and cannot exist.');
+      res.send(404, 'Defined party does not meet requirements: ' + req.params.party);
+    }
+
     models.event.findAll({
       where: { party: req.params.party }
     }).then((events) => {

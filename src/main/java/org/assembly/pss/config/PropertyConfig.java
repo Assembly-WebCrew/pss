6282c8795526bfd53f302357b3888d7c60986662
Joona Romppanen <joona@remod.fi>
2019-02-01 16:18:36 +0200

    Remove unused imports

--- src/main/java/org/assembly/pss/config/PropertyConfig.java
+++ src/main/java/org/assembly/pss/config/PropertyConfig.java
@@ -7,12 +7,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
